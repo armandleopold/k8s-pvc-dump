bed2c21646919a8cde5257ccf7076dbe0e8f65f2
Armand LEOPOLD <armand.leopold@hotmail.fr>
2020-12-22 12:55:21 +0100

    Update k8s-pvc-dump.py

--- k8s-pvc-dump.py
+++ k8s-pvc-dump.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pause
 import shutil
 import pandas as pd
@@ -135,7 +136,7 @@ while(True):
         dumpdf = path_to_df(DUMP_DIR)
         dumpdf['tobedel'] = dumpdf['created'].apply(dumpToDrop)
         print(tabulate(dumpdf, headers='keys', tablefmt='psql', showindex=False))
-        deletionList = list(dumpdf['path'][dumpdf['tobedel']])
+        deletionList = list(dumpdf['path'][dumpdf['tobedel'] & ~dumpdf['is_dir']])
         print("DUMPS to be deleted : "+str(deletionList))
         for dumpToDelete in deletionList:
             try:
